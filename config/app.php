af02372e067e41fd141454a15710736c7840001e
Max Nazarov <zettamax@gmail.com>
2017-04-11 15:03:28 +0300

    Install and setup IDE helper

--- config/app.php
+++ config/app.php
@@ -177,6 +177,9 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        /* IDE helper */
+        Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
+
     ],
 
     /*
