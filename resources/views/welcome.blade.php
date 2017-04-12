2fadba48a4d621a713232efe6522e8da7d49204c
Max Nazarov <zettamax@gmail.com>
2017-04-12 14:54:27 +0300

    Link javascript libs

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,6 +10,8 @@
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
 
+        <script src="/js/app.js"></script>
+
         <!-- Styles -->
         <style>
             html, body {
